542f38bacadce99a7a24ea959abf5ab0811a82ed
Deepak Kaul <dkaul1@jh.edu>
2016-04-01 14:25:08 -0400

    completed building of symbol tree lists

--- symtbl/table.go
+++ symtbl/table.go
@@ -7,20 +7,25 @@ import (
 )
 
 type Value struct {
-	MemLoc MemLoc
-	Lines  []int
+	MemLoc  MemLoc
+	SymType SymbolType
+	Lines   []int
 }
 
 type SymTbl map[string]*Value
 
 type SymTblLst struct {
 	SymTbl SymTbl
+	Name   string
 	Prev   *SymTblLst
 	Next   []*SymTblLst
 }
 
-var CurSymTblLst *SymTblLst
 var GlbSymTblLst *SymTblLst
+var CurSymTblLst *SymTblLst
+var TblSep string = "$"
+var GblName string = "$global"
+var InnerName string = "$inner"
 
 func NewSymTblLst(prev *SymTblLst) *SymTblLst {
 	s := new(SymTblLst)
@@ -34,10 +39,13 @@ func NewSymTblLst(prev *SymTblLst) *SymTblLst {
 func NewGlbSymTblLst() {
 	GlbSymTblLst = new(SymTblLst)
 	GlbSymTblLst.SymTbl = make(SymTbl)
+	GlbSymTblLst.Name = GblName
 	GlbSymTblLst.Prev = nil
 	GlbSymTblLst.Next = nil
-	GlbSymTblLst.SymTbl.Insert("input", -1)
-	GlbSymTblLst.SymTbl.Insert("output", -1)
+	input := syntree.NewStmtFunctionInputNode()
+	output := syntree.NewStmtFunctionOutputNode()
+	GlbSymTblLst.Insert(input)
+	GlbSymTblLst.Insert(output)
 	CurSymTblLst = GlbSymTblLst
 }
 
@@ -51,7 +59,7 @@ func PrintTableList(lst *SymTblLst) {
 	depth++
 	if lst != nil {
 		tbl := lst.SymTbl
-		fmt.Printf("    Scope level %+v\n", depth)
+		fmt.Printf("    Scope %+v level %+v\n", lst.Name, depth)
 		tbl.PrintTable()
 		for _, t := range lst.Next {
 			PrintTableList(t)
@@ -70,20 +78,30 @@ func Analyze(node syntree.Node) {
 
 func Insert(node syntree.Node) {
 	if node.(syntree.Symbol).Save() == true {
-		CurSymTblLst.SymTbl.Insert(node.(syntree.Name).Name(), node.Pos().Row())
-		log.AnalyzeLog.Printf("inserted %+v", node)
+		ok := CurSymTblLst.Insert(node)
+		if ok == true {
+			log.AnalyzeLog.Printf("inserted %+v into %+v", node, CurSymTblLst.Name)
+		}
+	} else {
+		log.AnalyzeLog.Printf("received %+v", node)
 	}
 	if node.(syntree.Symbol).AddScope() == true {
+		name := CurSymTblLst.Name
 		n := NewSymTblLst(CurSymTblLst)
 		CurSymTblLst = n
-		log.AnalyzeLog.Printf("added scoped symbol table")
+		if _, ok := node.(syntree.Name); ok {
+			CurSymTblLst.Name = name + TblSep + node.(syntree.Name).Name()
+		} else {
+			CurSymTblLst.Name = name + InnerName
+		}
+		log.AnalyzeLog.Printf("created %+v", CurSymTblLst.Name)
 	}
 }
 
 func Popout(node syntree.Node) {
 	if node.(syntree.Symbol).AddScope() == true {
+		log.AnalyzeLog.Printf("returned %+v", CurSymTblLst.Name)
 		CurSymTblLst = CurSymTblLst.Prev
-		log.AnalyzeLog.Printf("returned from scoped symbol table")
 	}
 }
 
@@ -94,28 +112,69 @@ func (s *SymTbl) PrintTable() {
 	fmt.Printf("    Variable Name Type Memory Location Lines\n")
 	fmt.Printf("    ============= ==== =============== =====\n")
 	for i, e := range *s {
-		fmt.Printf("    %+v\t 0x%08x\t %+v\n", i, e.MemLoc, e.Lines)
+		fmt.Printf("    %+v\t %+v 0x%08x\t %+v\n", i, e.SymType, e.MemLoc, e.Lines)
 	}
 	fmt.Printf("\n")
 }
 
-func (s *SymTbl) Insert(variable string, line int) {
-	table := *s
-	_, ok := table[variable]
-	if ok == true {
-		table[variable].Lines = append(table[variable].Lines, line)
-	} else {
-		table[variable] = new(Value)
-		if line != -1 {
-			table[variable].Lines = append(table[variable].Lines, line)
-		}
-		table[variable].MemLoc = glbMemLoc
-		glbMemLoc.Inc()
+func (s *SymTblLst) Insert(node syntree.Node) bool {
+	inserted := false
+
+	table := (*s).SymTbl
+	variable := node.(syntree.Name).Name()
+	line := node.Pos().Row()
+	symType := UNK_SYMBOL_TYPE
+
+	if node.(syntree.Symbol).IsFunc() {
+		symType = FUNCTION_TYPE
+	} else if node.(syntree.Symbol).IsArray() {
+		symType = ARRAY_TYPE
+	} else if node.(syntree.Symbol).IsInt() {
+		symType = INTEGER_TYPE
 	}
+
+	_, ok := table[variable]
+	if node.(syntree.Symbol).IsDeclaration() {
+		if ok == true {
+			log.ErrorLog.Printf(">>>> ERROR %+v already declared", variable)
+		} else {
+			table[variable] = new(Value)
+			if line != -1 {
+				table[variable].Lines = append(table[variable].Lines, line)
+			}
+			table[variable].SymType = symType
+			table[variable].MemLoc = glbMemLoc
+			glbMemLoc.Inc()
+			inserted = true
+		}
+	} else {
+		if ok == true {
+			table[variable].Lines = append(table[variable].Lines, line)
+			inserted = true
+		} else {
+			lst := s
+			for lst != nil {
+				tbl := lst.SymTbl
+				_, pOk := tbl[variable]
+				if pOk == true {
+					tbl[variable].Lines = append(tbl[variable].Lines, line)
+					inserted = true
+					break
+				}
+				log.AnalyzeLog.Printf("%+v", lst.Name)
+				lst = lst.Prev
+			}
+			if inserted == false {
+				log.ErrorLog.Printf(">>>> ERROR: %+v not declared", variable)
+			}
+		}
+	}
+
+	return inserted
 }
 
-func (s *SymTbl) Obtain(scope string, variable string) MemLoc {
-	table := *s
+func (s *SymTblLst) Obtain(scope string, variable string) MemLoc {
+	table := (*s).SymTbl
 	_, ok := table[variable]
 	if ok == true {
 		return table[variable].MemLoc
